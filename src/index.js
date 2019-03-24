4288b80554c162e1d279741e1a4a23319d58943d
alwAlone <gidm.teacher@gmail.com>
2019-03-24 14:00:19 +0300

    master/feat: add brackets code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let openBrackets = [];
+	let closeBrackets = [];
+	let stack   = [];
+
+	bracketsConfig.forEach(element => {
+    openBrackets.push(element[0]);
+		closeBrackets.push(element[1]);
+    });
+
+	for (let i = 0; i < str.length; i++) {
+		let currentBracket = str[i];
+
+		if (openBrackets.includes(currentBracket) && closeBrackets.includes(currentBracket)) {
+			if (stack.includes(currentBracket)) {
+				stack.pop()
+			} else {
+				stack.push(currentBracket);
+			}
+			continue;
+		}
+
+		if (openBrackets.includes(currentBracket)) {
+            stack.push(currentBracket);
+		} else {
+            let previousBracket = stack.pop();
+			if (openBrackets.indexOf(previousBracket) !== closeBrackets.indexOf(currentBracket)) {
+				return false;
+            }
+		}
+	}
+
+	return stack.length ? false : true;
 }
